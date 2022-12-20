2c3869eaf8ed4c5cf1fc0605a4e11562b97120ef
Vibhurathore09 <vibhurathoreshic@gmail.com>
2022-12-20 11:35:25 +0530

    init

---
+++ JS/PS1.js
@@ -0,0 +1,19 @@
+//Create a variable of type string and try to add a number to it
+    let a = "Harry" 
+    let b = 6
+    console.log(a + b)
+
+//Use typeof operator  to find out type of a+b
+    console.log(typeof (a+b));
+
+const ab = {
+    name : "Harry",
+    section : 1,
+    isPrincipal : false
+}
+// ab = 54 
+// console.log(ab)
+
+//Q4
+ab['friend'] = "Shubham"
+console.log(ab)
