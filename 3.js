92fe3709e4b56c6fce280bc386fa204099824ad8
Vibhurathore09 <vibhurathoreshic@gmail.com>
2022-12-20 11:21:03 +0530

    init

---
+++ 3.js
@@ -0,0 +1,10 @@
+console.log("JavaScript tutorial 3 : var , let and const")
+var a = 45;
+let b = "Vibhu";
+var c = null;
+var d = undefined;
+{
+    let b = "this";
+    console.log(b);
+}
+console.log(b);
---
+++ 4.js
@@ -0,0 +1,11 @@
+//Primitives and Objects
+//NN BB SS U
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt("567")+BigInt("3");
+let e = "Vibhu";
+let f = Symbol("I am a nice symbol");
+let g = undefined;
+console.log(a  ,b ,c ,d ,e, f, g);
+console.log(typeof d)
